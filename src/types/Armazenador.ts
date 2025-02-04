6ca3764e2533e708b9c743ae605e74f8768d79fa
Gerson Aparecido <gerson.lima@yduqs.com.br>
2025-02-04 16:40:48 -0300

    Entrega da semana-02-gerson

--- src/types/Armazenador.ts
+++ src/types/Armazenador.ts
@@ -2,6 +2,7 @@ export class Armazenador {
     private constructor() { }
 
     static salvar(chave: string, valor: any): void {
+        // Uma função em JavaScript usada para converter um valor JavaScript em uma string JSON
         const valorComoString = JSON.stringify(valor);
         localStorage.setItem(chave, valorComoString);
     }
@@ -16,6 +17,7 @@ export class Armazenador {
         }
 
         if (reviver) {
+        //Convertendo um array para string JSON            
             return JSON.parse(valor, reviver) 
         }
         return JSON.parse(valor);
